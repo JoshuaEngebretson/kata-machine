0ac13edef6ec4c9dbe7cf4ce76111a700ec21f71
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-09-15 09:20:21 -0500

    Added runtime notes to ArrayList.ts

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,27 +1,23 @@
 export default class ArrayList<T> {
     public length: number;
 
-    
+    constructor() {}
 
-    constructor() {
-    }
+    // add a new node at the end - O(1)
+    prepend(item: T): void {}
 
-    prepend(item: T): void {
+    // add a new node at the beginning - O(1)
+    append(item: T): void {}
 
-}
-    insertAt(item: T, idx: number): void {
+    // add a new node at a specific position - O(n)
+    insertAt(item: T, idx: number): void {}
 
-}
-    append(item: T): void {
+    // remove a node at a specific position - O(n)
+    remove(item: T): T | undefined {}
 
-}
-    remove(item: T): T | undefined {
+    // remove a node at a specific position - O(n)
+    removeAt(idx: number): T | undefined {}
 
-}
-    get(idx: number): T | undefined {
-
-}
-    removeAt(idx: number): T | undefined {
-
-}
+    // get the node by its index - O(n)
+    get(idx: number): T | undefined {}
 }
